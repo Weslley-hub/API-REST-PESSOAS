2d39e994994f7af437d2d1aea65e2b55d49a2903
Weslley Teixeira <weslleyfernando71609@gmail.com>
2024-04-01 11:54:49 -0300

    Metodo para criar pessoa

--- Sistemas/src/app/create-pessoa/create-pessoa.component.ts
+++ Sistemas/src/app/create-pessoa/create-pessoa.component.ts
@@ -1,12 +1,63 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterOutlet } from '@angular/router';
+import { Pessoa } from '../models/pessoa.model';
+import { PessoaService } from '../pessoa.service';
+import { Endereco } from '../models/endereco.model';
 
 @Component({
   selector: 'app-create-pessoa',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './create-pessoa.component.html',
   styleUrl: './create-pessoa.component.css'
 })
 export class CreatePessoaComponent {
+  
+  constructor(private pessoaService: PessoaService, private router: Router) { }
 
+  newPessoa: Pessoa = {
+    id: 0, 
+    nome: '',
+    sexo: '',
+    estadoCivil: '',
+    dataNascimento: '',
+    enderecos: [] 
+  };
+
+  newEndereco: Endereco = {
+    cep: '',
+    endereco: '',
+    numero: '',
+    complemento: '',
+    bairro: '',
+    estado: '',
+    cidade: ''
+  };
+
+  addEndereco() {
+    this.newPessoa.enderecos.push(this.newEndereco);
+    this.resetEndereco();
+  }
+
+  createPessoa() {
+    this.pessoaService.createPessoa(this.newPessoa).subscribe(() => {
+      this.router.navigate(['/']);
+    }, error => {
+      alert('Erro  ao criar a pessoa!' + error);
+    });
+  }
+
+  private resetEndereco() {
+    this.newEndereco = {
+      cep: '',
+      endereco: '',
+      numero: '',
+      complemento: '',
+      bairro: '',
+      estado: '',
+      cidade: '' 
+    };
+  }
 }
